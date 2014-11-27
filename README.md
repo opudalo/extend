8573bc06d43b93f8e9da69d9cde037f72b4293b7
Eugene Chechurin <e.chechurin@gmail.com>
2014-11-28 00:02:48 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## extend-es6
+  ES6 deep extend module
+
+### Installation
+
+    $ bower install --save zheneva/extend-es6
+
+### Examples
+
+```js
+
+extend({ a: 1, b: 5 }, { c: 3, b: { a: 1 } })
+//{ 
+//  a: 1,
+//  b: {
+//    a: 1
+//  },
+//  c: 3
+//}
+
+```
---
+++ index.es6.js
@@ -0,0 +1,12 @@
+export default function extend (obj) {
+  var args = Array.prototype.slice.call(arguments, 1)
+  
+  for (let arg of args) {
+    if (!arg) continue
+    for (let prop in arg) {
+      if (typeof obj[prop] == 'object') obj[prop] = extend(obj[prop], arg[prop])
+      else obj[prop] = arg[prop]
+    }
+  }
+  return obj
+}
